982d5bf245aa528441d49df5a326acc3765ad7bc
Will Powers <wpowers42@gmail.com>
2020-11-27 14:49:30 -0700

    clock format updates, animate ampm

--- js/clock.js
+++ js/clock.js
@@ -6,13 +6,14 @@ const clock = () => {
   const d = new Date();
   let hours = d.getHours();
   let minutes = d.getMinutes();
-  let ampm = hours >= 12 ? 'PM' : 'AM';
+  let ampm = hours >= 12 ? 'P' : 'A';
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
   hours = ('0' + hours).slice(-2); // single digit hours should have leading 0
   minutes = ('0' + minutes).slice(-2); // single digit minutes should have leading 0
   document.getElementById("hours").innerHTML = hours;
-  document.getElementById("ampm").innerHTML = ampm;
+  document.querySelector('.ampm-top').innerHTML = ampm;
+  document.querySelector('.ampm-bottom').innerHTML = 'M';
   document.getElementById("minutes").innerHTML = minutes;
 }
 
