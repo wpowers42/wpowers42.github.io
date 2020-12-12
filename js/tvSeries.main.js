65944be687bcd3626830831a4f4403229112b77c
Will Powers <wpowers42@gmail.com>
2020-12-12 02:36:30 -0700

    added mini series to json

--- js/tvSeries.main.js
+++ js/tvSeries.main.js
@@ -254,23 +254,23 @@ class Heatmap {
     }
   }
 
-const fuzzyhound = new FuzzySearch({ output_limit: 6, output_map: "alias" });
+/* eslint-disable no-underscore-dangle */
 
+// eslint-disable-next-line no-undef
+const fuzzyhound = new FuzzySearch({ output_limit: 6, output_map: 'alias' });
 
 // - - - - - - - - - - - - - - - - - - - - - - -
 //    Item template
 // - - - - - - - - - - - - - - - - - - - - - - -
 
 function itemTemplate(suggestion) {
-
   return [
-    "<div>",
-    "<span class='title'>\"", fuzzyhound.highlight(suggestion.title, "title"), "\"</span>",
-    "<span class='author'>", (suggestion.author) ? " by " + fuzzyhound.highlight(suggestion.author, "author") : "", "</span>",
-    "<span class='score' title='", suggestion._match, "'>(&nbsp;", suggestion._score.toFixed(2), "&nbsp;)</span>",
-    "</div>"
-  ].join("");
-
+    '<div>',
+    "<span class='title'>\"", fuzzyhound.highlight(suggestion.title, 'title'), '"</span>',
+    "<span class='author'>", (suggestion.author) ? ` by ${fuzzyhound.highlight(suggestion.author, 'author')}` : '', '</span>',
+    "<span class='score' title='", suggestion._match, "'>(&nbsp;", suggestion._score.toFixed(2), '&nbsp;)</span>',
+    '</div>',
+  ].join('');
 }
 
 //
@@ -278,56 +278,50 @@ function itemTemplate(suggestion) {
 //
 
 function footerTemplate() {
-  var render_end = Date.now();
-  return "<div class='typeahead-footer'> " +
-    "Search took <strong>" + fuzzyhound.search_time.toFixed(3) + " ms</strong><br>" +
-    "Total (search+highlight+render):  <strong>" + (render_end - fuzzyhound.start_time).toFixed(3) + " ms</strong><br>" +
-    "Query: <strong>" + fuzzyhound.query.normalized.length + "</strong> chars <br>" +
-    "Source contain " + fuzzyhound.source.length + " Items<br>" +
-    "</div>";
+  const renderEnd = Date.now();
+  return `${"<div class='typeahead-footer'> "
+    + 'Search took <strong>'}${fuzzyhound.search_time.toFixed(3)} ms</strong><br>`
+    + `Total (search+highlight+render):  <strong>${(renderEnd - fuzzyhound.start_time).toFixed(3)} ms</strong><br>`
+    + `Query: <strong>${fuzzyhound.query.normalized.length}</strong> chars <br>`
+    + `Source contain ${fuzzyhound.source.length} Items<br>`
+    + '</div>';
 }
 
-//- - - - - - - - - - -
+const handleSubmit = (suggestion) => {
+  const { tconst } = suggestion._item;
+  const url = `https://willp.herokuapp.com/tv-series/${tconst.toString()}`;
+  Heatmap(url);
+};
+
+// - - - - - - - - - - -
 //   Typeahead Setup
 // - - - - - - - - - - -
 
 $('#demo-query').typeahead({
   minLength: 2,
-  highlight: false //we'll use FuzzySearch highlight instead
+  highlight: false, // we'll use FuzzySearch highlight instead
 },
-  {
-    name: 'books',
-    limit: 100, // let fuzzyhound.output_limit apply limit
-    source: fuzzyhound,
-    display: "title",
-    templates: {
-      suggestion: itemTemplate,
-      footer: footerTemplate,
-      pending: ""
-    }
+{
+  name: 'books',
+  limit: 100, // let fuzzyhound.output_limit apply limit
+  source: fuzzyhound,
+  display: 'title',
+  templates: {
+    suggestion: itemTemplate,
+    footer: footerTemplate,
+    pending: '',
+  },
 
-  }).bind('typeahead:select', function (ev, suggestion) {
-    document.activeElement.blur();
-    handleSubmit(suggestion);
-  });
+}).bind('typeahead:select', (ev, suggestion) => {
+  document.activeElement.blur();
+  handleSubmit(suggestion);
+});
 
 fetch('https://raw.githubusercontent.com/wpowers42/wpowers42.github.io/main/json/series.json')
-  .then(res => res.json())
-  .then(data => {
+  .then((res) => res.json())
+  .then((data) => {
     fuzzyhound.setOptions({
       source: data,
-      keys: { title: 'title' }
+      keys: { title: 'title' },
     });
   });
-
-// - - - - - - - - - - - -
-//   Setup onsubmit handlers
-//- - - - - - - - - - - - -
-
-function handleSubmit(suggestion) {
-
-  var tconst = suggestion._item.tconst;
-  const url = `https://willp.herokuapp.com/tv-series/${tconst.toString()}`;
-  var hm = new Heatmap(url);
-
-}
